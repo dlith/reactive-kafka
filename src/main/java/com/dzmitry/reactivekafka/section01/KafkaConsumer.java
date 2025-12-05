8857c15488b6b21cb0981c74a230cc40acdf08e0
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-12-05 19:08:10 +0100

    creating kafka receiver

--- src/main/java/com/dzmitry/reactivekafka/section01/KafkaConsumer.java
+++ src/main/java/com/dzmitry/reactivekafka/section01/KafkaConsumer.java
@@ -1,24 +1,32 @@
 package com.dzmitry.reactivekafka.section01;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
-import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class KafkaConsumer {
 
     public static void main(String[] args) {
 
-        Map<String, ? extends Serializable> consumerConfig = Map.of(
+        Map<String, Object> consumerConfig = Map.of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.GROUP_ID_CONFIG, "demo-group"
         );
-        //ReceiverOptions.create()
-        //KafkaReceiver.create()
+
+        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver.create(options)
+                .receive()
+                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
+                .subscribe();
     }
 }
