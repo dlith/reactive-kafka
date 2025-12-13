0170de765146ca1dda7d1b45217f0b87995f483c
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-12-13 14:33:30 +0100

    cooperative sticky assignor

---
+++ src/main/java/com/dzmitry/reactivekafka/section06/KafkaConsumer.java
@@ -0,0 +1,37 @@
+package com.dzmitry.reactivekafka.section06;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class KafkaConsumer {
+
+    public static void start(String instanceId) {
+
+        Map<String, Object> consumerConfig = Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group-123",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, instanceId,
+                ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName()
+        );
+
+        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver.create(options)
+                .receive()
+                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
+                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .subscribe();
+    }
+}
