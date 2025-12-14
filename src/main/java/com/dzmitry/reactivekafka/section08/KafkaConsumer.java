aad882cb1b488097594ded87dbe3aea42da6b81c
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2025-12-14 11:43:43 +0100

    kafka with 3 replicas demo

---
+++ src/main/java/com/dzmitry/reactivekafka/section08/KafkaConsumer.java
@@ -0,0 +1,35 @@
+package com.dzmitry.reactivekafka.section08;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class KafkaConsumer {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> consumerConfig = Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:8081",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+
+        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver.create(options)
+                .receive()
+                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
+                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .subscribe();
+    }
+}
