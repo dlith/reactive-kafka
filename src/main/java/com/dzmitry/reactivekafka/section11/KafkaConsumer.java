671afc62fae27c34e2cde1e7910bd6a4052ad484
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2026-02-15 12:04:58 +0100

    grouped flex demo

---
+++ src/main/java/com/dzmitry/reactivekafka/section11/KafkaConsumer.java
@@ -0,0 +1,52 @@
+package com.dzmitry.reactivekafka.section11;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.GroupedFlux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.kafka.receiver.ReceiverRecord;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class KafkaConsumer {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> consumerConfig = Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+
+        ReceiverOptions<String, String> options = ReceiverOptions.<String, String>create(consumerConfig)
+                .commitInterval(Duration.ofSeconds(1))
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver.create(options)
+                .receive()
+                .groupBy(r -> Integer.parseInt(r.key()) % 5)
+                .flatMap(KafkaConsumer::batchProcess)
+                .subscribe();
+    }
+
+    private static Mono<Void> batchProcess(GroupedFlux<Integer, ReceiverRecord<String, String>> flux) {
+        return flux
+                .publishOn(Schedulers.boundedElastic())
+                .doFirst(() -> log.info("--------- mode: {}", flux.key()))
+                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
+                .then(Mono.delay(Duration.ofSeconds(1)))
+                .then();
+    }
+}
---
+++ src/main/java/com/dzmitry/reactivekafka/section11/KafkaProducer.java
@@ -0,0 +1,37 @@
+package com.dzmitry.reactivekafka.section11;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.Map;
+
+@Slf4j
+public class KafkaProducer {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> config = Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+
+        SenderOptions<String, String> senderOptions = SenderOptions.create(config);
+
+        Flux<SenderRecord<String, String, String>> flux =
+                Flux.range(1, 100)
+                        .map(i -> new ProducerRecord<>("order-events", i.toString(), "order-" + i))
+                        .map(pr -> SenderRecord.create(pr, pr.key()));
+
+        KafkaSender.create(senderOptions)
+                .send(flux)
+                .doOnNext(r -> log.info("correlation id: {}", r.correlationMetadata()))
+                .subscribe();
+    }
+}
