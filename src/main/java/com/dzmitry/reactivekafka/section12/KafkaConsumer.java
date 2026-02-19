53eed3b191774eb38533a68409b215670edd96da
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2026-02-19 18:53:33 +0100

    Simple retry

---
+++ src/main/java/com/dzmitry/reactivekafka/section12/KafkaConsumer.java
@@ -0,0 +1,39 @@
+package com.dzmitry.reactivekafka.section12;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class KafkaConsumer {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> consumerConfig = Map.of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+
+        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver.create(options)
+                .receive()
+                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value().toString().toCharArray()[15])) // exception just for demo
+                .doOnError(ex -> log.error(ex.getMessage()))
+                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(1)))
+                .blockLast(); //just for demo
+    }
+}
